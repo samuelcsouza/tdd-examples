47dbee79d7a8690843f1cdb91e42497fabbd109b
samuelcsouza <samuel_dacruz@hotmail.com>
2022-09-06 13:24:54 -0300

    refactor: change tests names

--- capitulo3/numeros_romanos/conversor_test.py
+++ capitulo3/numeros_romanos/conversor_test.py
@@ -3,38 +3,38 @@ from capitulo3.numeros_romanos.conversor import ConversorNumeroRomano
 
 class TestConversorNumerosRomanos():
 
-    def test_entender_o_simbolo_I(self):
+    def test_convert_symbol_I(self):
         conversor = ConversorNumeroRomano()
 
         numero = conversor.converte("I")
 
         assert 1 == numero
 
-    def test_entender_o_simbolo_V(self):
+    def test_convert_symbol_V(self):
         conversor = ConversorNumeroRomano()
         numero = conversor.converte("V")
 
         assert 5 == numero
 
-    def test_entender_dois_simbolos_como_II(self):
+    def test_convert_symbol_II(self):
         conversor = ConversorNumeroRomano()
         numero = conversor.converte("II")
 
         assert 2 == numero
 
-    def test_entender_quatro_simbolos_como_XXII(self):
+    def test_convert_symbol_XXII(self):
         conversor = ConversorNumeroRomano()
         numero = conversor.converte("XXII")
 
         assert 22 == numero
 
-    def test_entender_numeros_como_IX(self):
+    def test_convert_symbol_IX(self):
         conversor = ConversorNumeroRomano()
         numero = conversor.converte("IX")
 
         assert 9 == numero
 
-    def test_entender_numeros_como_XXIV(self):
+    def test_convert_symbol_XXIV(self):
         conversor = ConversorNumeroRomano()
         numero = conversor.converte("XXIV")
 
